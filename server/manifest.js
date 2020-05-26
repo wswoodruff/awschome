f77ce81ecfbb022cd2a7e0b1eaa0f7af6132f2af
wswoodruff <williamswoodruff@gmail.com>
2020-05-26 18:19:21 -0400

    Init commit :smile:

---
+++ server/manifest.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+const Toys = require('toys');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {
+                    region: process.env.AWS_REGION,
+                    s3: {
+                        accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+                        secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY
+                    }
+                }
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
